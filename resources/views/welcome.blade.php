9a588ac44713c163e6e6a717ab0e11237ec8bd3d
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-05-05 14:31:20 +0700

    add: login page, dan animasi

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,13 @@
         you to build your TailwindCSS from the source.
         https://tailwindcss.com/docs/installation
     -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.9.2/tailwind.min.css">
+    <!-- Tailwind CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- AOS CSS -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
     <!-- Small CSS to Hide elements at 1520px size -->
+
     <style>
         @media(max-width:1520px) {
             .left-svg {
@@ -151,7 +156,8 @@
 
 
     <!-- BEGIN HERO SECTION -->
-    <div class="relative items-center justify-center w-full overflow-x-hidden lg:pt-40 lg:pb-40 xl:pt-40 xl:pb-64">
+    <div class="relative items-center justify-center w-full overflow-x-hidden lg:pt-40 lg:pb-40 xl:pt-40 xl:pb-64"
+        data-aos="fade-up" data-aos-delay="400">
         <div
             class="container flex flex-col items-center justify-between h-full max-w-6xl px-8 mx-auto -mt-32 lg:flex-row xl:px-0">
             <div
@@ -184,7 +190,7 @@
                                 <path
                                     d="M1175 0C525.8 0 0 525.8 0 1175c0 552.2 378.9 1010.5 890.1 1139.7-5.9-14.7-8.8-35.3-8.8-55.8v-199.8H734.4c-79.3 0-152.8-35.2-185.1-99.9-38.2-70.5-44.1-179.2-141-246.8-29.4-23.5-5.9-47 26.4-44.1 61.7 17.6 111.6 58.8 158.6 120.4 47 61.7 67.6 76.4 155.7 76.4 41.1 0 105.7-2.9 164.5-11.8 32.3-82.3 88.1-155.7 155.7-190.9-393.6-47-581.6-240.9-581.6-505.3 0-114.6 49.9-223.3 132.2-317.3-26.4-91.1-61.7-279.1 11.8-352.5 176.3 0 282 114.6 308.4 143.9 88.1-29.4 185.1-47 284.9-47 102.8 0 196.8 17.6 284.9 47 26.4-29.4 132.2-143.9 308.4-143.9 70.5 70.5 38.2 261.4 8.8 352.5 82.3 91.1 129.3 202.7 129.3 317.3 0 264.4-185.1 458.3-575.7 499.4 108.7 55.8 185.1 214.4 185.1 331.9V2256c0 8.8-2.9 17.6-2.9 26.4C2021 2123.8 2350 1689.1 2350 1175 2350 525.8 1824.2 0 1175 0z" />
                             </g>
-                            </g>
+                            {{-- </g> --}}
                         </svg>
                         <svg class="h-8 mr-4 text-gray-500 duration-150 cursor-pointer fill-current transition-color hover:text-gray-600"
                             viewBox="0 0 2500 2158" xmlns="http://www.w3.org/2000/svg">
@@ -261,7 +267,8 @@
     <!-- HERO SECTION END -->
 
     <!-- BEGIN FEATURES SECTION -->
-    <div id="features" class="relative w-full px-8 py-10 border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0">
+    <div id="features" class="relative w-full px-8 py-10 border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0"
+        data-aos="fade-up" data-aos-delay="400">
         <div class="container flex flex-col items-center justify-between h-full max-w-6xl mx-auto">
             <h2 class="my-5 text-base font-medium tracking-tight text-indigo-500 uppercase">Our Features</h2>
             <h3
@@ -465,7 +472,8 @@
     <!-- END FEATURES SECTION -->
 
     <!-- Pricing Section -->
-    <div class="relative px-8 py-10 bg-white border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0">
+    <div class="relative px-8 py-10 bg-white border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0"
+        data-aos="fade-up" data-aos-delay="400">
 
         <div id="pricing" class="container flex flex-col items-center h-full max-w-6xl mx-auto">
             <h2 class="my-5 text-base font-medium tracking-tight text-indigo-500 uppercase">Our Pricing</h2>
@@ -668,7 +676,8 @@
 
     <!-- Start Testimonials -->
     <div id="testimonials"
-        class="flex items-center justify-center w-full px-8 py-10 border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0">
+        class="flex items-center justify-center w-full px-8 py-10 border-t border-gray-200 md:py-16 lg:py-24 xl:py-40 xl:px-0"
+        data-aos="fade-up" data-aos-delay="400">
         <div class="max-w-6xl mx-auto">
             <div class="flex-col items-center ">
                 <div class="flex flex-col items-center justify-center w-full h-full max-w-2xl pr-8 mx-auto text-center">
@@ -882,8 +891,16 @@
                 href="https://themewagon.com/">Themewagon</a></div>
     </footer>
 
-    <!-- a little JS for the mobile nav button -->
+
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+
     <script>
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-out-back',
+            once: true // Mencegah animasi ulang saat scroll kembali
+        });
+
         window.addEventListener("scroll", function () {
             var header = document.querySelector("header");
             if (window.scrollY > 0) {
