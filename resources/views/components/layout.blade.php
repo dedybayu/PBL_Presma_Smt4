929bfb43ef749b24a72cfd210a8fe9ed16485ee1
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-05-06 13:01:05 +0700

    update

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -57,7 +57,7 @@
     <!--CORE-->
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.js')}}delivr.net/npm/metismenu"></script>
+    <script src="https://cdn.jsdelivr.net/npm/metismenu"></script>
     <script src="{{ asset('assets/js/scripts-init/app.js')}}"></script>
     <script src="{{ asset('assets/js/scripts-init/demo.js')}}"></script>
 
@@ -186,7 +186,7 @@
     <!--TABLES -->
     <!--DataTables-->
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.js')}}delivr.net/npm/datatables.net-bs4@1.10.19/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/datatables.net-bs4@1.10.19/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/dataTables.responsive.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/responsive/2.2.3/js/responsive.bootstrap.min.js" crossorigin="anonymous"></script>
 
