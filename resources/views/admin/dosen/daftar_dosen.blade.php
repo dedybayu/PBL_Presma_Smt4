8ca2b536939270359545fb1a85b20ac3ad8d9a7a
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-05-14 07:45:29 +0700

    update: bug dosen

--- resources/views/admin/dosen/daftar_dosen.blade.php
+++ resources/views/admin/dosen/daftar_dosen.blade.php
@@ -55,13 +55,6 @@
         </div>
     </div>
 
-    {{-- Modal Container --}}
-    <div id="modal-crud" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
-        data-keyboard="false" aria-hidden="true">
-        <div class="modal-dialog modal-lg" role="document">
-            <div class="modal-content"></div>
-        </div>
-    </div>
 
     <x-slot:js>
         <script>
