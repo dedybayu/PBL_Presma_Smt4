6af334d0b45a54d4adad533f739dc69dc76c3918
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-05-15 23:30:02 +0700

    update: route

--- routes/web.php
+++ routes/web.php
@@ -31,68 +31,58 @@ Route::post('/login', [AuthController::class, 'login'])->name('custom.login');
 Route::middleware('auth')->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index']);
 
-    Route::middleware(['role:ADM'])->group(function () {
-
+    Route::middleware(['role:ADM,DOS'])->group(function () {
+        Route::prefix('mahasiswa')->group(function () {
+            Route::get('/', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
+            Route::post('/list', [MahasiswaController::class, 'list']);
+            Route::get('/{id}/show', [MahasiswaController::class, 'show'])->name('mahasiswa.show');
+            Route::get('/create', [MahasiswaController::class, 'create'])->name('mahasiswa.create');
+            Route::post('/', [MahasiswaController::class, 'store'])->name('mahasiswa.store');
+            Route::get('/{id}/edit', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
+            Route::put('/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
+            Route::delete('/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.destroy');
+        });
+        Route::prefix('kelas')->group(function () {
+            Route::get('/', [KelasController::class, 'index'])->name('kelas.index');
+            Route::post('/list', [KelasController::class, 'list']);
+            Route::get('/{id}/show', [KelasController::class, 'show'])->name('kelas.show');
+            Route::get('/create', [KelasController::class, 'create'])->name('kelas.create');
+            Route::post('/', [KelasController::class, 'store'])->name('kelas.store');
+            Route::get('/{kelas}/edit', [KelasController::class, 'edit'])->name('kelas.edit');
+            Route::put('/{kelas}', [KelasController::class, 'update'])->name('kelas.update');
+            Route::delete('/{kelas}', [KelasController::class, 'destroy'])->name('kelas.destroy');
+        });
+        Route::prefix('dosen')->group(function () {
+            Route::get('/', [DosenController::class, 'index'])->name('dosen.index');
+            Route::post('/list', [DosenController::class, 'list']);
+            Route::get('/{id}/show', [DosenController::class, 'show'])->name('dosen.show');
+            Route::get('/create', [DosenController::class, 'create'])->name('dosen.create');
+            Route::post('/', [DosenController::class, 'store'])->name('dosen.store');
+            Route::get('/{id}/edit', [DosenController::class, 'edit'])->name('dosen.edit');
+            Route::put('/{id}', [DosenController::class, 'update'])->name('dosen.update');
+            Route::delete('/{id}', [DosenController::class, 'destroy'])->name('dosen.destroy');
+        });
+        Route::prefix('admin')->group(function () {
+            Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+            Route::post('/list', [AdminController::class, 'list'])->name('admin.list');
+            Route::get('/create', [AdminController::class, 'create'])->name('admin.create');
+            Route::post('/', [AdminController::class, 'store'])->name('admin.store');
+            Route::get('/{id}/edit', [AdminController::class, 'edit'])->name('admin.edit');
+            Route::put('/{id}', [AdminController::class, 'update'])->name('admin.update');
+            Route::delete('/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
+        });
+        Route::prefix('prodi')->group(function () {
+            Route::get('/', [ProdiController::class, 'index'])->name('prodi.index');
+            Route::post('/list', [ProdiController::class, 'list'])->name('prodi.list');
+            Route::get('/create', [ProdiController::class, 'create'])->name('prodi.create');
+            Route::post('/', [ProdiController::class, 'store'])->name('prodi.store');
+            Route::get('/{id}/edit', [ProdiController::class, 'edit'])->name('prodi.edit');
+            Route::put('/{id}', [ProdiController::class, 'update'])->name('prodi.update');
+            Route::delete('/{id}', [ProdiController::class, 'destroy'])->name('prodi.destroy');
+        });
     });
 });
 
-
-
-
-Route::middleware(['role:MHS,DOS,ADM'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index']);
-});
-
-Route::middleware(['role:ADM'])->group(function () {
-    Route::prefix('mahasiswa')->group(function () {
-        Route::get('/', [MahasiswaController::class, 'index'])->name('mahasiswa.index');
-        Route::post('/list', [MahasiswaController::class, 'list']);
-        Route::get('/{id}/show', [MahasiswaController::class, 'show'])->name('mahasiswa.show');
-        Route::get('/create', [MahasiswaController::class, 'create'])->name('mahasiswa.create');
-        Route::post('/', [MahasiswaController::class, 'store'])->name('mahasiswa.store');
-        Route::get('/{id}/edit', [MahasiswaController::class, 'edit'])->name('mahasiswa.edit');
-        Route::put('/{id}', [MahasiswaController::class, 'update'])->name('mahasiswa.update');
-        Route::delete('/{id}', [MahasiswaController::class, 'destroy'])->name('mahasiswa.destroy');
-    });
-    Route::prefix('kelas')->group(function () {
-        Route::get('/', [KelasController::class, 'index'])->name('kelas.index');
-        Route::post('/list', [KelasController::class, 'list']);
-        Route::get('/{id}/show', [KelasController::class, 'show'])->name('kelas.show');
-        Route::get('/create', [KelasController::class, 'create'])->name('kelas.create');
-        Route::post('/', [KelasController::class, 'store'])->name('kelas.store');
-        Route::get('/{kelas}/edit', [KelasController::class, 'edit'])->name('kelas.edit');
-        Route::put('/{kelas}', [KelasController::class, 'update'])->name('kelas.update');
-        Route::delete('/{kelas}', [KelasController::class, 'destroy'])->name('kelas.destroy');
-    });
-    Route::prefix('dosen')->group(function () {
-        Route::get('/', [DosenController::class, 'index'])->name('dosen.index');
-        Route::post('/list', [DosenController::class, 'list']);
-        Route::get('/{id}/show', [DosenController::class, 'show'])->name('dosen.show');
-        Route::get('/create', [DosenController::class, 'create'])->name('dosen.create');
-        Route::post('/', [DosenController::class, 'store'])->name('dosen.store');
-        Route::get('/{id}/edit', [DosenController::class, 'edit'])->name('dosen.edit');
-        Route::put('/{id}', [DosenController::class, 'update'])->name('dosen.update');
-        Route::delete('/{id}', [DosenController::class, 'destroy'])->name('dosen.destroy');
-    });
-    Route::prefix('admin')->group(function () {
-        Route::get('/', [AdminController::class, 'index'])->name('admin.index');
-        Route::post('/list', [AdminController::class, 'list'])->name('admin.list');
-        Route::get('/create', [AdminController::class, 'create'])->name('admin.create');
-        Route::post('/', [AdminController::class, 'store'])->name('admin.store');
-        Route::get('/{id}/edit', [AdminController::class, 'edit'])->name('admin.edit');
-        Route::put('/{id}', [AdminController::class, 'update'])->name('admin.update');
-        Route::delete('/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
-    });
-    Route::prefix('prodi')->group(function () {
-        Route::get('/', [ProdiController::class, 'index'])->name('prodi.index');
-        Route::post('/list', [ProdiController::class, 'list'])->name('prodi.list');
-        Route::get('/create', [ProdiController::class, 'create'])->name('prodi.create');
-        Route::post('/', [ProdiController::class, 'store'])->name('prodi.store');
-        Route::get('/{id}/edit', [ProdiController::class, 'edit'])->name('prodi.edit');
-        Route::put('/{id}', [ProdiController::class, 'update'])->name('prodi.update');
-        Route::delete('/{id}', [ProdiController::class, 'destroy'])->name('prodi.destroy');
-    });
-});
 
 
 
