1c1dda43934da844e4f35d560f2acb951642aa2d
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-05-16 12:01:12 +0700

    update: layout edit mahasiswa

--- resources/views/admin/mahasiswa/edit_mahasiswa.blade.php
+++ resources/views/admin/mahasiswa/edit_mahasiswa.blade.php
@@ -27,69 +27,100 @@
             </button>
         </div>
         <div class="modal-body">
-            <div class="text-center">
-                <img id="profileImage" class="img-thumbnail rounded-circle mb-3"
-                    style="width: 160px; height: 160px; object-fit: cover;"
-                    src="{{ $mahasiswa->profile_picture ? asset('storage/' . auth()->mahasiswa()->profile_picture) : asset('img/user.png') }}"
-                    alt="Profile picture">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="text-center">
+                        <img id="profileImage" class="img-thumbnail rounded-circle mb-3"
+                            style="width: 160px; height: 160px; object-fit: cover;"
+                            src="{{ $mahasiswa->profile_picture ? asset('storage/' . auth()->mahasiswa()->profile_picture) : asset('assets/images/user.png') }}"
+                            alt="Profile picture">
 
-                <div class="mt-2">
-                    <input type="file" id="profile_picture" name="profile_picture" class="d-none" accept="image/*"
-                        onchange="previewImage(event)">
-                    <button type="button" onclick="document.getElementById('profile_picture').click()"
-                        class="btn btn-primary">
-                        Change Picture
-                    </button>
-                    <button type="button" onclick="removeImage()" class="btn btn-outline-danger">
-                        Delete Picture
-                    </button>
+                        <div class="mt-2">
+                            <input type="file" id="profile_picture" name="profile_picture" class="d-none" accept="image/*"
+                                onchange="previewImage(event)">
+                            <button type="button" onclick="document.getElementById('profile_picture').click()"
+                                class="btn btn-primary">
+                                Change Picture
+                            </button>
+                            <button type="button" onclick="removeImage()" class="btn btn-outline-danger">
+                                Delete Picture
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <input type="hidden" id="remove_picture" name="remove_picture" value="0">
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Username</label>
+                        <input value="{{ $mahasiswa->user->username }}" type="text" name="username" id="username"
+                            class="form-control" required>
+                        <small id="error-username" class="error-text form-text text-danger"></small>
+                    </div>
+                    <div class="form-group">
+                        <label>NIM</label>
+                        <input value="{{ $mahasiswa->nim }}" type="number" name="nim" id="nim" class="form-control"
+                            required>
+                        <small id="error-nim" class="error-text form-text text-danger"></small>
+                    </div>
+                    <div class="form-group">
+                        <label>Nama</label>
+                        <input value="{{ $mahasiswa->nama }}" type="text" name="nama" id="nama" class="form-control"
+                            required>
+                        <small id="error-nama" class="error-text form-text text-danger"></small>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input value="{{ $mahasiswa->email }}" type="email" name="email" id="email" class="form-control"
+                            required>
+                        <small id="error-email" class="error-text form-text text-danger"></small>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>No. Tlp</label>
+                        <input value="{{ $mahasiswa->no_tlp }}" type="text" name="no_tlp" id="no_tlp" class="form-control"
+                            required>
+                        <small id="error-no_tlp" class="error-text form-text text-danger"></small>
+                    </div>
                 </div>
             </div>
-            <input type="hidden" id="remove_picture" name="remove_picture" value="0">
 
-            <div class="form-group">
-                <label>Username</label>
-                <input value="{{ $mahasiswa->user->username }}" type="text" name="username" id="username" class="form-control"
-                    required>
-                <small id="error-username" class="error-text form-text text-danger"></small>
+                <div class="form-group">
+                    <label>Alamat</label>
+                    <input value="{{ $mahasiswa->alamat }}" type="text" name="alamat" id="alamat" class="form-control"
+                        required>
+                    <small id="error-alamat" class="error-text form-text text-danger"></small>
+                </div>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Tahun Angkatan</label>
+                        <input value="{{ $mahasiswa->tahun_angkatan }}" type="number" name="tahun_angkatan"
+                            id="tahun_angkatan" class="form-control" required>
+                        <small id="error-tahun_angkatan" class="error-text form-text text-danger"></small>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input value="" type="password" name="password" id="password" class="form-control">
+                        <small class="form-text text-muted">Abaikan jika tidak ingin ubah password</small>
+                        <small id="error-password" class="error-text form-text text-danger"></small>
+                    </div>
+                </div>
             </div>
-            <div class="form-group">
-                <label>NIM</label>
-                <input value="{{ $mahasiswa->nim }}" type="number" name="nim" id="nim" class="form-control"
-                    required>
-                <small id="error-nim" class="error-text form-text text-danger"></small>
-            </div>
-            <div class="form-group">
-                <label>Nama</label>
-                <input value="{{ $mahasiswa->nama }}" type="text" name="nama" id="nama" class="form-control" required>
-                <small id="error-nama" class="error-text form-text text-danger"></small>
-            </div>
-            <div class="form-group">
-                <label>Email</label>
-                <input value="{{ $mahasiswa->email }}" type="email" name="email" id="email" class="form-control" required>
-                <small id="error-email" class="error-text form-text text-danger"></small>
-            </div>
-            <div class="form-group">
-                <label>No. Tlp</label>
-                <input value="{{ $mahasiswa->no_tlp }}" type="text" name="no_tlp" id="no_tlp" class="form-control" required>
-                <small id="error-no_tlp" class="error-text form-text text-danger"></small>
-            </div>
-            <div class="form-group">
-                <label>Alamat</label>
-                <input value="{{ $mahasiswa->alamat }}" type="text" name="alamat" id="alamat" class="form-control" required>
-                <small id="error-alamat" class="error-text form-text text-danger"></small>
-            </div>
-            <div class="form-group">
-                <label>Tahun Angkatan</label>
-                <input value="{{ $mahasiswa->tahun_angkatan }}" type="number" name="tahun_angkatan" id="tahun_angkatan" class="form-control" required>
-                <small id="error-tahun_angkatan" class="error-text form-text text-danger"></small>
-            </div>
-            <div class="form-group">
-                <label>Password</label>
-                <input value="" type="password" name="password" id="password" class="form-control">
-                <small class="form-text text-muted">Abaikan jika tidak ingin ubah password</small>
-                <small id="error-password" class="error-text form-text text-danger"></small>
-            </div>
+
+
+
+
+
+
+
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-warning" data-dismiss="modal">Batal</button>
