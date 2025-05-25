30c91f173a701f07e5fe0acd889909045ea50f74
Fandy Hanz <hanzyura28@gmail.com>
2025-05-25 21:16:36 +0700

    add: show lomba display pamflet

--- resources/views/admin/lomba/show_lomba.blade.php
+++ resources/views/admin/lomba/show_lomba.blade.php
@@ -1,4 +1,5 @@
 @empty($lomba)
+    {{-- This section is for when $lomba is empty (data not found) --}}
     <div id="modal-delete" class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
@@ -14,9 +15,15 @@
                 </div>
                 <a href="{{ url('/lomba') }}" class="btn btn-warning">Kembali</a>
             </div>
+            {{-- For the 'not found' case, the footer with a close button might not be strictly necessary if 'Kembali' sends them away --}}
+            {{-- But if you want a close button here too, you'd add: --}}
+            {{-- <div class="modal-footer">
+                <button type="button" data-dismiss="modal" class="btn btn-primary btn-sm">Close</button>
+            </div> --}}
         </div>
     </div>
 @else
+    {{-- This section is for when $lomba is NOT empty (data found) --}}
     <div class="modal-header">
         <h5 class="modal-title">Data lomba</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -46,7 +53,7 @@
                 <td class="col-9">{{ $lomba->penyelenggara->penyelenggara_nama }}</td>
             </tr>
             <tr>
-                <th class="text-right col-3">tanggal mulai  :</th>
+                <th class="text-right col-3">tanggal mulai :</th>
                 <td class="col-9">{{ $lomba->tanggal_mulai }}</td>
             </tr>
             <tr>
@@ -54,7 +61,27 @@
                 <td class="col-9">{{ $lomba->tanggal_selesai }}</td>
             </tr>
         </table>
-        <div class="modal-footer">
-            <button type="button" data-dismiss="modal" class="btn btn-primary btn-sm">Close</button>
+        <div class="row">
+            <div class="col-md-6 mt-2">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Foto Sertifikat</h5>
+                        <div
+                            style="position: relative; width: 100%; max-width: 600px; aspect-ratio: 16 / 9; overflow: hidden; background: #eee;">
+                            <a href="{{ asset('storage/' . $lomba->foto_pamflet) }}" target="_blank">
+                                <img id="preview-pamflet" src="{{ asset('storage/' . $lomba->foto_pamflet) }}"
+                                    alt="Pamflet"
+                                    style="width: 100%; height: 100%; object-fit: contain; display: block;">
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div> {{-- End of modal-body --}}
+
+    {{-- The modal-footer should be directly inside modal-content, after modal-body --}}
+    <div class="modal-footer">
+        <button type="button" data-dismiss="modal" class="btn btn-primary btn-sm">Close</button>
+    </div>
 @endempty
