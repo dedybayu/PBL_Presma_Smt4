b6b595821f746fd2d0b8c01679260b35f18b3b29
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-05-31 11:24:07 +0700

    add: profile

--- resources/views/mahasiswa/profile_mahasiswa.blade.php
+++ resources/views/mahasiswa/profile_mahasiswa.blade.php
@@ -30,11 +30,71 @@
             <div class="tab-content">
                 <div class="tab-pane active" id="tab-eg115-0" role="tabpanel">
                     <div class="tab-profile ">
-                        <h1>Iki Profil</h1>
 
-                        <div class="d-flex justify-content-end">
-                            <button class="btn btn-success" onclick="editProfile()"><i class="fa fa-edit"></i> Edit</button>
+                        {{-- PROFILE --}}
+                        <div class="bg-light" style="height: 250px; overflow: hidden;">
+                            <img src="{{ asset('assets/images/gdungjti2.png') }}" class="w-100 h-100 object-fit-cover"
+                                style="object-fit: cover;">
                         </div>
+
+                        <div class="text-center mt-n5">
+                            <img src="{{ asset(auth()->user()->mahasiswa->foto_profile ? 'storage/' . auth()->user()->mahasiswa->foto_profile : 'assets/images/user.png') }}"
+                                class="rounded-circle border border-primary shadow bg-white"
+                                style="width: 150px; height: 150px; object-fit: cover;">
+                            <div class="d-flex justify-content-center align-items-center mt-2">
+                                <h4 class="mb-0 me-2">{{ auth()->user()->mahasiswa->nama }}</h4>
+                            </div>
+                            <p class="text-muted mt-2">{{ auth()->user()->mahasiswa->nim }}</p>
+                            <span class="badge bg-info text-dark mb-3">Mahasiswa</span>
+                        </div>
+
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Nama Mahasiswa</label>
+                                    <div type="text" class="form-control" style="background-color: #e9ecef">
+                                        {{ auth()->user()->mahasiswa->nama }}</div>
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">NIM Mahasiswa</label>
+                                    <div type="text" class="form-control" style="background-color: #e9ecef">
+                                        {{ auth()->user()->mahasiswa->nim }}</div>
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Username</label>
+                                    <div type="text" class="form-control" style="background-color: #e9ecef">
+                                        {{ auth()->user()->username }}</div>
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Email</label>
+                                    <div type="text" class="form-control" style="background-color: #e9ecef">
+                                        {{ auth()->user()->mahasiswa->email }}</div>
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Kelas</label>
+                                    <div type="text" class="form-control" style="background-color: #e9ecef">
+                                        {{ auth()->user()->mahasiswa->kelas->kelas_nama }}</div>
+                                </div>
+                                <div class="mb-3 col-md-6">
+                                    <label class="form-label">Angkatan</label>
+                                    <div type="text" class="form-control" style="background-color: #e9ecef">
+                                        {{ auth()->user()->mahasiswa->tahun_angkatan }}</div>
+                                </div>
+                                <div class="mb-3 col-md-12">
+                                    <label class="form-label">Alamat</label>
+                                    <div type="text" class="form-control" style="background-color: #e9ecef">
+                                        {{ auth()->user()->mahasiswa->alamat }}</div>
+                                </div>
+                            </div>
+
+                            <div class="d-flex justify-content-end">
+                                <button class="btn btn-success" onclick="editProfile()"><i class="fa fa-edit"></i>
+                                    Edit</button>
+                            </div>
+
+                        </div>
+                        {{-- /PROFILE --}}
+
                     </div>
 
                 </div>
@@ -75,8 +135,8 @@
         <h1>Edit Profile</h1>
 
         <div class="d-flex justify-content-between">
-        <button class="btn btn-warning" onclick="cancelEditProfile()">Batal</button>
-        <button class="btn btn-success" onclick><i class="fa fa-save"></i> Simpan</button>
+            <button class="btn btn-warning" onclick="cancelEditProfile()">Batal</button>
+            <button class="btn btn-success" onclick><i class="fa fa-save"></i> Simpan</button>
         </div>
     </template>
 
