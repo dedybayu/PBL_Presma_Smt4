0367b3fc9bd4c87d178155945915eca6cfb47a66
Dedy Bayu Setiawan <dedybayusetiawan5@gmail.com>
2025-06-07 22:37:40 +0700

    fix: footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,18 +1,11 @@
 <div class="app-wrapper-footer">
     <div class="app-footer">
-        <div class="">
-            <div class="row" style="width: 100%;">
-                <div class="col-2 d-flex align-items-center justify-content-center" style="">
-                    <img src="{{ asset('assets/images/presapp-text-logo.png') }}" alt="" class="img-fluid"
-                        style="max-height: 50px;">
-                </div>
+        <div class="d-flex align-items-center justify-content-center" style="width: 100%;">
 
-                <div class="col-10 d-flex align-items-center" style="">
-                    <span class="text-muted">&copy; 2025 presma.dbsnetwork.my.id</span>
-                </div>
-            </div>
+            <img src="{{ asset('assets/images/presapp-text-logo.png') }}" alt="" class="img-fluid"
+                style="max-height: 35px;">
+
+            <span class="text-muted ml-3">&copy; 2025 presma.dbsnetwork.my.id</span>
         </div>
     </div>
 </div>
-
-
