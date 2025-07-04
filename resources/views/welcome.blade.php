b519d7f9751bd992b6b219244e1896d8e468656f
Michelle Dorani Shiba <doranimichelle@gmail.com>
2025-07-04 13:30:35 +0700

    remove: unneeded parts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/png" href="{{ asset('assets/images/presapp-logo.png') }}" />
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 
     <title>PresApp</title>
 
